23b3323b0b1897cdc32eb15b796a841c1d89ca41
John Henry Galino <github.stolze@simplelogin.com>
2023-03-27 04:29:30 +0800

    initial framework for filters

---
+++ js/gameFilterFunctions.js
@@ -0,0 +1,8 @@
+function getFilterOptions () {
+  const platforms = document.querySelectorAll('.platform>div>input:checked');
+  const categories = document.querySelectorAll('.category>div>input:checked');
+  const tags = document.querySelectorAll('.tags>div>input:checked');
+  const sortBy = document.querySelectorAll('.sortBy>option');
+
+  console.log(platforms);
+}
