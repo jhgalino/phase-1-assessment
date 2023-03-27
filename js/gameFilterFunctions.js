5ca6f7517e0b3b5797cab5806c7d3d5a6df3b491
John Henry Galino <github.stolze@simplelogin.com>
2023-03-27 08:44:31 +0800

    add function to retrieve filters

--- js/gameFilterFunctions.js
+++ js/gameFilterFunctions.js
@@ -22,3 +22,32 @@ function getFilterOptions () {
   };
 }
 
+async function fetchFilteredItems (filters) {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'c40d428cdamshc9610d296a3fda7p107c53jsncebbbe3a4c5c',
+      'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
+    },
+  };
+
+  let platform;
+  if (filters.platforms.length === 2) {
+    platform = '&platform=all';
+  } else if (filters.platforms === 1) {
+    platform = `&platform=${filters.platforms[0]}`;
+  } else {
+    platform = '';
+  }
+
+  const sort = filters.sortBy[0] ? `&sort-by=${filters.sortBy[0]}` : '';
+
+  const tags = filters.tags.concat(filters.categories).length
+    ? filters.tags.concat(filters.categories).join('.')
+    : '';
+
+  const data = await fetch(`https://free-to-play-games-database.p.rapidapi.com/
+api/filter?tag=${tags}&${platform}&${sort}`, options);
+
+  return await data.json();
+}
